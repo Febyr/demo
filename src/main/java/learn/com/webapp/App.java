8d15b531c11042a6badbb8af76116fed653995eb
fraju <fraju@gmail.com>
2024-03-28 23:14:46 +0000

    Added new project folder

---
+++ src/main/java/learn/com/webapp/App.java
@@ -0,0 +1,16 @@
+package learn.com.webapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to DevOps" );
+        System.out.println( "This simple java class which compiles, package and deploy" );
+        
+        
+    }
+}
